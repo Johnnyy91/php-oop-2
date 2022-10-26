b34fc19eed02bec9b955fea232bebbfb4e4a3273
Johnnyy91 <claudiosardilli@gmail.com>
2022-10-26 15:13:29 +0200

    file creation

--- shop.php
+++ shop.php
@@ -6,11 +6,7 @@ I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
 L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
 Il pagamento avviene con la carta di credito, che non deve essere scaduta */
 
-class Food {};
 
-class Toys {};
-
-class Credit_card {};
 
 
 ?>
